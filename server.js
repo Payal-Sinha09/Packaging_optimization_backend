303627564729626a91d7983129d035404e9beef2
Payal-Sinha09 <payal09apr@gmail.com>
2024-09-25 22:30:05 +0530

    changes

--- server.js
+++ server.js
@@ -104,6 +104,7 @@ app.use(express.json());
 
 // Connect to MongoDB using the URI from environment variable
 const mongoURI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully.'))
@@ -206,6 +207,11 @@ app.post('/optimize-packaging', async (req, res) => {
 });
 
 // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
