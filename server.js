6534a82a1e58aa9b8231c8df90d90d89d19a5d80
Payal-Sinha09 <payal09apr@gmail.com>
2024-09-25 22:30:39 +0530

    changes

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ const dotenv = require('dotenv'); // Import dotenv
 dotenv.config({ path: 'config/uri.env' });
 
 const app = express();
-const PORT = 5000;
+// const PORT = 5000;
 
 // Middleware
 app.use(cors());
